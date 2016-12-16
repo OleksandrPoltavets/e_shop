35d04581687bab408fcf7b6f2985f063245d3288
Oleksandr Poltavets <sancho.ck@gmail.com>
2016-12-17 00:34:46 +0200

    show product in modal with AJAX

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,17 @@
 class ProductsController < ApplicationController
+  before_action :set_product, except: [:index]
 
   def index
     @products = Product.all
   end
 
   def show
+  end
+
+  private
+
+  def set_product
+    @product = Product.find_by(id: params[:id])
   end
 
 end
